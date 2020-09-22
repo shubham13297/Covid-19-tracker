a2608a9cd45835e2f3dbc89d01892a82097ec97a
shubham <sy13021997@gmail.com>
2020-09-22 18:07:28 +0530

    Updates

---
+++ precache-manifest.75a89580eb5ef188e4c7de39ac526655.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e961a584404fc6643b9f3de1aa28cea0",
+    "url": "/Covid-19-tracker/index.html"
+  },
+  {
+    "revision": "288effcefcd22c7d145c",
+    "url": "/Covid-19-tracker/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "9fd293bbdcaaee017020",
+    "url": "/Covid-19-tracker/static/css/main.41bfbba7.chunk.css"
+  },
+  {
+    "revision": "288effcefcd22c7d145c",
+    "url": "/Covid-19-tracker/static/js/2.b0a70698.chunk.js"
+  },
+  {
+    "revision": "ebc70c097554e9e2ef968c42fad2beba",
+    "url": "/Covid-19-tracker/static/js/2.b0a70698.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fd293bbdcaaee017020",
+    "url": "/Covid-19-tracker/static/js/main.5107f810.chunk.js"
+  },
+  {
+    "revision": "93c322cfa2ae1d702392",
+    "url": "/Covid-19-tracker/static/js/runtime-main.008c7622.js"
+  }
+]);
